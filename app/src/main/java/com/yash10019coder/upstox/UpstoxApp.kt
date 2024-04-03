371b868d1084890d5634882b25eb1c272e1887e3
yash10019coder <yashverma10019@gmail.com>
2024-04-04 04:44:18 +0530

    created expandable view for displaying total stake values.

--- app/src/main/java/com/yash10019coder/upstox/UpstoxApp.kt
+++ app/src/main/java/com/yash10019coder/upstox/UpstoxApp.kt
@@ -1,5 +1,6 @@
 package com.yash10019coder.upstox
 
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -9,5 +10,7 @@ class UpstoxApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
---
+++ app/src/main/java/com/yash10019coder/upstox/ui/databinding/StockTotalListingsModel.kt
@@ -0,0 +1,8 @@
+package com.yash10019coder.upstox.ui.databinding
+
+data class StockTotalListingsModel(
+    val totalCurrentValue: Double,
+    val totalInvestment: Double,
+    val todayProfitAndLoss: Double,
+    val profitAndLoss: Double
+)
